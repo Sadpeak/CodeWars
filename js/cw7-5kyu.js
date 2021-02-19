b4b4eb81e9a7e4b051e4c5cbe9fa9af74569da07
Sadpeak <maxstack@mail.ru>
2021-02-19 15:11:58 +0300

    kata #7

---
+++ js/cw7-5kyu.js
@@ -0,0 +1,9 @@
+// link to kata: https://www.codewars.com/kata/5255892719453db6d2000a23/train/javascript
+
+
+var anarchy = function () {
+    let i = true;
+    return 'I can write numbers like, ' + i++ + ', ' + i++ + ', ' + i + '.';
+}
+
+module.exports = anarchy;
---
+++ js/cw7-5kyu.test.js
@@ -0,0 +1,5 @@
+const anachar = require('./cw7-5kyu');
+
+test('anachar', () => {
+    expect(anachar()).toBe('I can write numbers like, 1, 2, 3.')
+})
